d22c3f48bd90e3045cae0c03bb98f067bbb898cb
shackney <s.hackney@pitt.edu>
2018-02-09 12:34:30 -0500

    json, pinterest, etc

---
+++ pinapi.py
@@ -0,0 +1,11 @@
+# app id 4949203845075970405
+
+
+https://api.pinterest.com/v1/me/pins/?access_token=4949203845075970405&fields=id,creator,note&limit=1
+
+ https://api.pinterest.com/oauth/?
+    response_type=code&
+    redirect_uri=https://mywebsite.com/connect/pinterest/&
+    client_id=4949203845075970405&
+    scope=read_public,write_public&
+    state=768uyFys
---
+++ rijksapi.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import urllib3
+#  jmkt4GYt is the key
+response = requests.get("https://www.rijksmuseum.nl/api/usersets/1774320--ocarine/collections/jewelry?key=jmkt4GYt&format=json")
+
+# Print the content of the response.
+print(response)
+# parameters = {"lat": 37.78, "lon": -122.41}
+# response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+#
+# # Get the response data as a python object.  Verify that it's a dictionary.
+# data = response.json()
+# print(type(data))
+# print(data)
